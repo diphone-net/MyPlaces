b1202b31e0502481022fa970fd426e25125163be
user143580 <user143580@eu253.local>
2018-10-05 15:15:52 +0100

    Adaptar DetailControler i Main.storyboard (escena de detalle) per cumplir ama la pantalla de PLA1

--- MyPlaces/DetailController.swift
+++ MyPlaces/DetailController.swift
@@ -8,23 +8,44 @@
 
 import UIKit
 
-class DetailController: UIViewController {
+class DetailController: UIViewController , UIPickerViewDelegate, UIPickerViewDataSource{
+    
+    // obligat per ViewDataSource
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1 //num de columnes
+    }
+
+    // obligat per ViewDataSource
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return placesTypes.count // num d'elements
+    }
+    
+    // necessari per VciewDataSource
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return placesTypes[row]
+    }
+    
+    // controla el canvi al pickerView
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {        
+        showTuristicMode(isTuristic: row == 1)
+    }
 
     @IBOutlet weak var constraintHeight: NSLayoutConstraint!
     
     @IBOutlet weak var txtName: UITextField!
-    @IBOutlet weak var txtDescription: UITextField!
     @IBOutlet weak var txtId: UITextField!
     @IBOutlet weak var txtDiscount: UITextField!
     @IBOutlet weak var lblDiscount: UILabel!
-    @IBOutlet weak var segType: UISegmentedControl!
     @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var txtNotes: UITextView!
     
     @IBOutlet weak var btnUndo: UIButton!
     @IBOutlet weak var btnRandom: UIButton!
     @IBOutlet weak var btnSave: UIButton!
     @IBOutlet weak var btnRemove: UIButton!
     
+    @IBOutlet weak var pkrType: UIPickerView!
+    let placesTypes = ["Generic place","Touristic place"]
     
     var m_provider = ManagerPlaces.share()
     
@@ -33,38 +54,51 @@ class DetailController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.pkrType.delegate = self
+        self.pkrType.dataSource = self
+        
         // sempre arribem aqui amb place assignat pero pel que fos, crido aquesta funcio si fos que no
         self.constraintHeight.constant = 400
         
         enableEdition(enable: place == nil)
         if (place != nil){
-            txtName.text = place!.name
-            txtDescription.text = place!.description
-            txtId.text = place!.id
+            fillData()
+        }else{
             showTuristicMode(isTuristic: false)
-            if (place!.type != PlaceTourist.PlacesTypes.GenericPlace){
-                let touristPlace = place as! PlaceTourist
-                txtDiscount.text = touristPlace.discount_tourist
-                showTuristicMode(isTuristic: true)
-            }
-            // TODO: segur que aixo es pot fer en 1 linia
-            if (place!.image != nil){
-                img.image = UIImage(data: place!.image!)
-            }
+        }
+    }
+    
+    private func fillData(){
+        txtName.text = place!.name
+        txtNotes.text = place!.description
+        txtId.text = place!.id
+        showTuristicMode(isTuristic: false)
+        pkrType.selectRow(0, inComponent: 0, animated: true)
+        if (place!.type != PlaceTourist.PlacesTypes.GenericPlace){
+            let touristPlace = place as! PlaceTourist
+            txtDiscount.text = touristPlace.discount_tourist
+            showTuristicMode(isTuristic: true)
+        }
+        // TODO: segur que aixo es pot fer en 1 linia
+        if (place!.image != nil){
+            img.image = UIImage(data: place!.image!)
         }
     }
     
     private func showTuristicMode(isTuristic: Bool){
         lblDiscount.isHidden = !isTuristic
         txtDiscount.isHidden = !isTuristic
-        segType.selectedSegmentIndex = isTuristic ? 1 : 0
+        //segType.selectedSegmentIndex = isTuristic ? 1 : 0
+        pkrType.selectRow(isTuristic ? 1 : 0, inComponent:0, animated: true)
+
     }
     
     private func enableEdition(enable: Bool){
         txtName.isEnabled = enable
-        txtDescription.isEnabled = enable
+        txtNotes.isUserInteractionEnabled = enable
         // txtId by desing
-        segType.isEnabled = enable
+        //segType.isEnabled = enable
+        pkrType.isUserInteractionEnabled = enable
         txtDiscount.isEnabled = enable
         btnUndo.isHidden = !enable
         btnRandom.isHidden = !enable
@@ -82,11 +116,12 @@ class DetailController: UIViewController {
     }
     
     @IBAction func saveTemporal(_ sender: Any) {
-        if segType.selectedSegmentIndex == 0 {
-            let place = Place(name: txtName.text!, description: txtDescription.text!, image_in: img.image?.pngData())
+        //if segType.selectedSegmentIndex == 0 {
+        if true { // todo
+            let place = Place(name: txtName.text!, description: txtNotes.text!, image_in: img.image?.pngData())
             m_provider.append(place)
         }else{
-            let tplace = PlaceTourist(name: txtName.text!, description: txtDescription.text!, discount_tourist: txtDiscount.text!, image_in: img.image?.pngData())
+            let tplace = PlaceTourist(name: txtName.text!, description: txtNotes.text!, discount_tourist: txtDiscount.text!, image_in: img.image?.pngData())
             m_provider.append(tplace)	
         }
         undoTemporal(sender)
@@ -105,7 +140,7 @@ class DetailController: UIViewController {
         var rand = Int(arc4random_uniform(UInt32(texts.count)))
         txtName.text = texts[rand]
         rand = Int(arc4random_uniform(UInt32(descripcions.count)))
-        txtDescription.text = descripcions[rand]
+        txtNotes.text = descripcions[rand]
         if (Int(arc4random_uniform(2)) == 1){
             rand = Int(arc4random_uniform(100))
             txtDiscount.text = String(rand)
